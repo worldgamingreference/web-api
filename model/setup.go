5e729f8e89bdfa29d670e609599db94262f20b16
Jeremy Chauvin <jeremychauvin10@gmail.com>
2020-11-28 14:05:19 +0100

    feat(): add env var

--- model/setup.go
+++ model/setup.go
@@ -14,7 +14,7 @@ var DB *gorm.DB
 
 // ConnectDataBase is connect to database with var env
 func ConnectDataBase() {
-	dsn := fmt.Sprintf("user=%s password=%s dbname=%s port=%s sslmode=disable host=%s", os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"), os.Getenv("DB_PORT"), os.Getenv("DB_HOST"))
+	dsn := fmt.Sprintf("user=%s password=%s dbname=%s port=%s sslmode=%s host=%s", os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"), os.Getenv("DB_PORT"), os.Getenv("DB_SSL_MODE"), os.Getenv("DB_HOST"))
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Info),
 	})
